ebeeb120d06a275d0c7d80b2b2da50bbc90a9234
oknerazan <oknerazano_cerezuk@mail.ru>
2015-09-08 19:02:49 +0300

    burito

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$a = (9);
+$b = (14);
+
+if ( $a > $b ){
+    echo $a." Bolche ".$b;
+}
+elseif($a == $b) {
+
+    echo $a . " Rowno " . $b;
+}
+else {
+    echo $a . " Menche " . $b;
+}
+
+echo rand() . "\n";
+echo rand() . "\n";
+
+echo rand(5, 15);
+
+
+2
+
+?>
